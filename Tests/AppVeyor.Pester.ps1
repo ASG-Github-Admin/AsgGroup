465df87e79421cdb9c0ea9bf9085d58d23cd8322
Robert Woodward <Robert.Woodward@asggroup.com.au>
2019-07-17 16:30:38 +1000

    Update AppVeyor.Pester.ps1
    Adding comments and improving code.

--- Tests/AppVeyor.Pester.ps1
+++ Tests/AppVeyor.Pester.ps1
@@ -1,7 +1,8 @@
 # Pester test invocation
 # Pester tests invoked, XML results serialised and pulled intp 'appveyor.yml'
 
-#If Finalize is specified, we collect XML output, upload tests, and indicate build errors
+# Parameters #
+
 param(
     
     # XML output collected, tests uploaded, indicate build errors
@@ -14,50 +15,68 @@ param(
     [string] $ProjectRoot = $env:APPVEYOR_BUILD_FOLDER
 )
 
-#Initialize some variables, move to the project root
-$TimeStamp = Get-Date -UFormat "%Y%m%d-%H%M%S"
-$PSMajVer = $PSVersionTable.PSVersion.Major
-$TestFile = "TestResults_PS$PSMajVer`_$TimeStamp.xml"
+# Variables #
 
+# Current date time stamp in UNIX format
+$TimeStamp = Get-Date -UFormat "%Y%m%d-%H%M%S"
+
+# PowerShell major version number
+$PSMajVer = $PSVersionTable.PSVersion.Major
+
+# XML test file name
+$TestFileName = "TestResults_PS$PSMajVer`_$TimeStamp.xml"
+
+# Appveyor job identifier uniform resource locator
 $URL = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+
+# Begin #
+
+# Location changed to project root
 Set-Location -Path $ProjectRoot
 
+# Verbose setting
 $Verbose = @{ }
+
+# Process #
+
+# Verbose enabled when branch is not like 'master'
 if ($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master") { $Verbose.Add("Verbose", $true) }
-   
+
 if ($Test) {
 
     Write-Host -Object "`n`tStatus: Testing with PowerShell $PSMajVer`n"
 
     Import-Module -Name Pester -Force
 
-    $TestFilePath = "$ProjectRoot\$TestFile"
+    $TestFilePath = "$ProjectRoot\$TestFileName"
     Invoke-Pester @Verbose -Path "$ProjectRoot\Tests" -OutputFormat NUnitXml -OutputFile $TestFilePath -PassThru |
     Export-Clixml -Path "$ProjectRoot\PesterResults_PS$PSMajVer`_$TimeStamp.xml"
     
     if ($env:APPVEYOR_JOB_ID) { ([System.Net.WebClient]::new()).UploadFile($URL, $TestFilePath) }
 }
 
-#If finalize is specified, display errors and fail build if we ran into any
 if ($Finalise) {
 
-    #Show status...
+    # Pester test result summary
     $AllFiles = Get-ChildItem -Path $ProjectRoot\PesterResults*.xml |
     Select-Object -ExpandProperty FullName
-    Write-Host -Object "`n`tStatus: Finalizing results`n"
+    Write-Host -Object "`n`tStatus: Finalising results`n"
     Write-Host -Object "Collating files:`n$($AllFiles | Out-String)"
 
-    #What failed?
+    # Result collection
     $Results = @(Get-ChildItem -Path "$ProjectRoot\PesterResults_PS*.xml" | Import-Clixml)
     
+    # Failed test check
     $FailedCount = $Results | Select-Object -ExpandProperty FailedCount |
     Measure-Object -Sum | Select-Object -ExpandProperty Sum
 
     if ($FailedCount -gt 0) {
 
+        # Failed item collection
         $FailedItems = $Results | Select-Object -ExpandProperty TestResult |
         Where-Object -FilterScript { $PSItem.Passed -notlike $true }
 
+        # Failed tests summary list
         Write-Host -Object "Failed tests summary:`n"
         $FailedItems | ForEach-Object -Process {
 
@@ -70,6 +89,7 @@ if ($Finalise) {
             }
         } | Sort-Object -Property Describe, Context, Name, Result | Format-List
 
+        # Build fail with test failure count
         throw "$FailedCount test(s) failed."
     }
 }
