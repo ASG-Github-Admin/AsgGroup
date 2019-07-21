af6dfbde74b8ec116d6916b4dd8d051aa27fc2b5
Robert Woodward <Robert.Woodward@asggroup.com.au>
2019-07-22 09:24:14 +1000

    Another fix

--- Tests/2.Function.Tests.ps1
+++ Tests/2.Function.Tests.ps1
@@ -128,8 +128,7 @@ Describe "'Out-LogFile' PowerShell $PSMajVer integration test" {
         }
 
         It "should have a properly formatted log entry" {
-
-            Out-LogFile -Path $FilePath -Level Error -Entry Test
+            
             $FilePath |
             Should -FileContentMatch "\[[0-2][0-9]:[0-5][0-9]:[0-6][0-9] [0-3]\d-[0-1]\d-\d{2}] Error: Test"
         }
